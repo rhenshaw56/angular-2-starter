88ec92a7d034a7381154aeb7d8149e3175da0de9
rowland henshaw <rowland.henshaw@gmail.com>
2018-07-04 02:18:17 +0100

    app entry module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './shared/app.routing';
+
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AdminModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
